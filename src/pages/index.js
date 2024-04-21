92e93b0e53ee7f7284d7d8b3023472491c840f42
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-04-21 17:09:46 -0300

    feat: organizing card elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,25 @@ import {
   Divider,
   CardFooter,
   Button,
+  CardHeader,
 } from "@chakra-ui/react";
 
 import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+// Vetor com URLs de imagens diferentes
+const images = [
+  "https://images.unsplash.com/photo-1617325247661-675ab4b64ae2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+  "https://images.unsplash.com/photo-1617325247661-675ab4b64ae2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+function getRandomImage() {
+  // Seleciona aleatoriamente um índice do vetor de imagens
+  const randomIndex = Math.floor(Math.random() * images.length);
+  // Retorna a URL da imagem correspondente ao índice selecionado
+  return images[randomIndex];
+}
+
 
 export default function Home() {
   return (
@@ -30,20 +46,17 @@ export default function Home() {
       <Container maxW="8xl" p="5em 0em">
         <SimpleGrid gap={50} minChildWidth="sm" justifyItems="center" justifyContent="center" alignItems="center">
           {[...Array(20)].map((_, index) => (
-            <Card key={index} maxW="sm" w="90vw">
+            <Card key={index} maxW="sm" w="85vw" alignItems="center">
               <CardBody>
                 <Image
-                  src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src={getRandomImage()}
                   alt="Green double couch with wooden legs"
                   borderRadius="lg"
+                  boxSize="20em"
+                  objectFit='fill'
                 />
-                <Stack mt="6" spacing="3">
-                  <Heading size="md">Living room Sofa</Heading>
-                  <Text>
-                    This sofa is perfect for modern tropical spaces, baroque
-                    inspired spaces, earthy toned spaces and for people who love
-                    a chic design with a sprinkle of vintage design.
-                  </Text>
+                <Stack mt="6" spacing="3" alignItems="center">
+                  <Heading size="md" fontWeight="400">Living room Sofa</Heading>
                   <Text color="blue.600" fontSize="2xl">
                     $450
                   </Text>
